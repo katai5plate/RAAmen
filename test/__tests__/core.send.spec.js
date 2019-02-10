149d13bbd3a0fc53e014563c536eb3f85b413b82
katai5plate <katai5plate.sub@gmail.com>
2019-02-11 00:14:37 +0900

    fix: テストコードのさらなる無駄ネストの削除

--- test/__tests__/core.send.spec.js
+++ test/__tests__/core.send.spec.js
@@ -2,43 +2,42 @@
 const _ = require('lodash');
 const connect = require('../connect');
 
-describe('core', () => {
+const files = [
+  'src/RAAmen_core.js',
+];
+
+describe('RAA.send()', () => {
   // const fail = () => expect().toEqual('FAILED');
-  const files = [
-    'src/RAAmen_core.js',
-  ];
-  describe('RAA.send()', () => {
-    it('存在する', () => {
-      connect({ files });
-      const { RAA } = window;
-      expect(RAA.send).toBeTruthy();
-    });
-    it('１回だけ送信 > 成功', async () => {
-      connect({ files });
-      const { RAA } = window;
-      const response = await RAA.send();
-      expect(response.result).toBeTruthy();
-      expect(response.error).toBeNull();
-    });
-    it('規定回数以上連続送信 > 失敗', async () => {
-      connect({ files });
-      const { RAA } = window;
-      const { falseMax } = RAA;
-      let response;
-      // 初期送信
+  it('存在する', () => {
+    connect({ files });
+    const { RAA } = window;
+    expect(RAA.send).toBeTruthy();
+  });
+  it('１回だけ送信 > 成功', async () => {
+    connect({ files });
+    const { RAA } = window;
+    const response = await RAA.send();
+    expect(response.result).toBeTruthy();
+    expect(response.error).toBeNull();
+  });
+  it('規定回数以上連続送信 > 失敗', async () => {
+    connect({ files });
+    const { RAA } = window;
+    const { falseMax } = RAA;
+    let response;
+    // 初期送信
+    response = await RAA.send();
+    expect(response.result).toBeTruthy();
+    expect(response.error).toBeNull();
+    // 規制直前まで送信
+    _.range(falseMax).forEach(async () => {
       response = await RAA.send();
       expect(response.result).toBeTruthy();
       expect(response.error).toBeNull();
-      // 規制直前まで送信
-      _.range(falseMax).forEach(async () => {
-        response = await RAA.send();
-        expect(response.result).toBeTruthy();
-        expect(response.error).toBeNull();
-      });
-      // 規制になる送信
-      response = await RAA.send();
-      expect(response.result).toBeFalsy();
-      expect(response.error.code).toEqual('API_CALL_LIMIT_EXCEEDED');
     });
+    // 規制になる送信
+    response = await RAA.send();
+    expect(response.result).toBeFalsy();
+    expect(response.error.code).toEqual('API_CALL_LIMIT_EXCEEDED');
   });
 });
