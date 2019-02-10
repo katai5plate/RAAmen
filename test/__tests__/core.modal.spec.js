c6cd42b0f7dc6a688caa725b9fcfb00eec69aa1f
katai5plate <katai5plate.sub@gmail.com>
2019-02-11 00:22:53 +0900

    feat: modalのテストにclient有無を追記

--- test/__tests__/core.modal.spec.js
+++ test/__tests__/core.modal.spec.js
@@ -19,15 +19,7 @@ describe('RAA.modal()', () => {
     const { RAA } = window;
     expect(RAA.modal).toBeTruthy();
   });
-  it('メッセージのみ : 送信 > 成功', async () => {
-    connect({ files });
-    const { RAA } = window;
-    global.alert = mockAlert;
-    const message = 'dummy';
-    await RAA.modal({ message });
-    expect(getMockAlertReturn()).toEqual(message);
-  });
-  it('メッセージなし : 送信 > 失敗', async () => {
+  it('引数なし : 送信 > 失敗', async () => {
     connect({ files });
     const { RAA } = window;
     global.alert = mockAlert;
@@ -39,46 +31,71 @@ describe('RAA.modal()', () => {
     }
     fail();
   });
-  it('checkValid : 通す > 成功', async () => {
-    connect({ files });
-    const { RAA } = window;
-    global.alert = mockAlert;
-    const message = 'dummy';
-    await RAA.modal({ message, checkValid: () => true });
-    expect(getMockAlertReturn()).toEqual(message);
-  });
-  it('checkValid : 通さない > BAD_REQUEST', async () => {
-    connect({ files });
-    const { RAA } = window;
-    global.alert = mockAlert;
-    const message = 'dummy';
-    try {
-      await RAA.modal({ message, checkValid: () => false });
-    } catch (error) {
-      expect(error.constructor.name).toEqual('AtsumaruApiError');
-      expect(error.code).toEqual('BAD_REQUEST');
-      return;
-    }
-    fail();
-  });
-  it('decorate : 変化する設定 > messageと異なる', async () => {
-    connect({ files });
-    const { RAA } = window;
-    global.alert = mockAlert;
-    const message = 'Hello';
-    const decorate = x => `${x}, World!`;
-    await RAA.modal({ message, decorate });
-    expect(getMockAlertReturn()).not.toEqual(message);
-    expect(getMockAlertReturn()).toEqual(decorate(message));
-  });
-  it('decorate : 変化しない設定 > messageと同じ', async () => {
-    connect({ files });
-    const { RAA } = window;
-    global.alert = mockAlert;
-    const message = 'Hello';
-    const decorate = x => x;
-    await RAA.modal({ message, decorate });
-    expect(getMockAlertReturn()).toEqual(message);
-    expect(getMockAlertReturn()).toEqual(decorate(message));
-  });
+  [undefined, true, false] // 内部処理と待機時間は変わるが、結果は同じになる
+    .forEach((client) => {
+      describe(`client : ${client}`, () => {
+        it('メッセージのみ : 送信 > 成功', async () => {
+          connect({ files });
+          const { RAA } = window;
+          global.alert = mockAlert;
+          const message = 'dummy';
+          await RAA.modal({ message, client });
+          expect(getMockAlertReturn()).toEqual(message);
+        });
+        it('メッセージなし : 送信 > 失敗', async () => {
+          connect({ files });
+          const { RAA } = window;
+          global.alert = mockAlert;
+          try {
+            await RAA.modal({ client });
+          } catch (error) {
+            expect(error.message).toEqual('message is undefined');
+            return;
+          }
+          fail();
+        });
+        it('checkValid : 通す > 成功', async () => {
+          connect({ files });
+          const { RAA } = window;
+          global.alert = mockAlert;
+          const message = 'dummy';
+          await RAA.modal({ message, checkValid: () => true, client });
+          expect(getMockAlertReturn()).toEqual(message);
+        });
+        it('checkValid : 通さない > BAD_REQUEST', async () => {
+          connect({ files });
+          const { RAA } = window;
+          global.alert = mockAlert;
+          const message = 'dummy';
+          try {
+            await RAA.modal({ message, checkValid: () => false, client });
+          } catch (error) {
+            expect(error.constructor.name).toEqual('AtsumaruApiError');
+            expect(error.code).toEqual('BAD_REQUEST');
+            return;
+          }
+          fail();
+        });
+        it('decorate : 変化する設定 > messageと異なる', async () => {
+          connect({ files });
+          const { RAA } = window;
+          global.alert = mockAlert;
+          const message = 'Hello';
+          const decorate = x => `${x}, World!`;
+          await RAA.modal({ message, decorate, client });
+          expect(getMockAlertReturn()).not.toEqual(message);
+          expect(getMockAlertReturn()).toEqual(decorate(message));
+        });
+        it('decorate : 変化しない設定 > messageと同じ', async () => {
+          connect({ files });
+          const { RAA } = window;
+          global.alert = mockAlert;
+          const message = 'Hello';
+          const decorate = x => x;
+          await RAA.modal({ message, decorate, client });
+          expect(getMockAlertReturn()).toEqual(message);
+          expect(getMockAlertReturn()).toEqual(decorate(message));
+        });
+      });
+    });
 });
